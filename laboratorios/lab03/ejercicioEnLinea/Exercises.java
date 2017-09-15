480cab977a8243a664076ceb0f3d4b070367959c
Agustin Nieto Garcia <anietog1@eafit.edu.co>
2017-09-14 22:40:32 -0500

    Soluciones a 2.1 y 2.2

--- laboratorios/lab03/ejercicioEnLinea/Exercises.java
+++ laboratorios/lab03/ejercicioEnLinea/Exercises.java
@@ -1,49 +1,78 @@
-import java.util.LinkedList;
+import java.util.*;
 
 public class Exercises{
-    public static void keyboardDisorder(String input) {
-	//O(3n), pero no se como expresarlo en el calculo...
-        LinkedList<String> list = new LinkedList<>();
+    //EL EJERCICIO DEL TECLADO (2.1)
+    public static LinkedList<StringBuilder> brokenKeyboard(String input) {
+        LinkedList<StringBuilder> list = new LinkedList<>();//C
 
-        boolean atFirst = true;
-        int i = 0;
-        while (i < input.length()) {
+        boolean atFirst = true;//C
+        StringBuilder sb = new StringBuilder();//C
 
-            if (!isBracket(input.charAt(i))) {
-                StringBuilder sb = new StringBuilder();
-
-                int j = i;
-                while (j < input.length() && !isBracket(input.charAt(j))) {
-                    sb.append(input.charAt(j));
-                    j++;
-                }
-
-                if (atFirst) {
-                    list.addFirst(sb.toString());
-                } else {
-                    list.addLast(sb.toString());
-                }
-
-                i = j;
+        int i = 0;//C
+        while (i < input.length()) {//n
+            if (!isBracket(input.charAt(i))){//n*C
+                sb.append(input.charAt(i));//n*C
             } else {
-                atFirst = input.charAt(i) == '[';
-                i++;
+                add(sb, list, atFirst);//n*C
+                sb = new StringBuilder();//n*C
+                atFirst = input.charAt(i) == '[';//n*C
             }
+            
+            i++;//n*C
         }
-
-        list.forEach((str) -> {
-            System.out.print(str);
-        });
-
-        System.out.println();
+        
+        if(sb.length() > 0) add(sb, list, atFirst);//C
+        return list;//C
+        //Complejidad O(n)
     }
-
-    private static boolean isBracket(char o) {
+    
+    private static void add(StringBuilder sb,
+			    LinkedList<StringBuilder> list, boolean atFirst){//O(1)
+        if (atFirst) list.addFirst(sb);
+        else list.addLast(sb);
+    }
+    
+    private static boolean isBracket(char o) {//O(1)
         return o == '[' || o == ']';
     }
 
+    public static void testKeyboard(){
+	Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        sc = new Scanner(line);
+        while(sc.hasNext()){
+            brokenKeyboard(sc.next()).forEach((str) -> {
+                System.out.print(str);
+            });
+            System.out.println();
+        }
+    }
+    
+    //EL EJERCICIO DE CODEFORCES
+    public static void codeForcesChallenge(){
+	Scanner sc = new Scanner(System.in);
+
+	String s = sc.next();
+	int n = sc.nextInt();
+
+	while(n > 0){
+	    int l = sc.nextInt() - 1;
+	    int r = sc.nextInt() - 1;
+	    int count = 0;
+	    
+	    for(int i=l; i<r; i++)
+		if(s.charAt(i) == s.charAt(i+1)) count++;//O(n)
+
+	    System.out.println(count);
+	    
+	    n--;
+	}
+    }
+    
     public static void main(String[] args){
-	keyboardDisorder("This_is_a_[Beiju]_text.");
-	keyboardDisorder("hola[Mundo, ], qué[[[] tal?");
+        System.out.println("PRUEBA BROKENKEYBOARD");
+	testKeyboard();
+	System.out.println("PRUEBA CODEFORCES");
+	codeForcesChallenge();
     }
 }
