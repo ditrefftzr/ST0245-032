2da627b38545672bd8929b98d4fceb669f5b5bd6
Agustin Nieto Garcia <anietog1@eafit.edu.co>
2017-09-17 15:28:23 -0500

    El ejercicio de codeforces no pasaba, [optimizado]*

--- laboratorios/lab03/ejercicioEnLinea/Exercises.java
+++ laboratorios/lab03/ejercicioEnLinea/Exercises.java
@@ -50,23 +50,30 @@ public class Exercises{
     
     //EL EJERCICIO DE CODEFORCES
     public static void codeForcesChallenge(){
-	Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-	String s = sc.next();
-	int n = sc.nextInt();
+        String s = sc.next();
 
-	while(n > 0){
-	    int l = sc.nextInt() - 1;
-	    int r = sc.nextInt() - 1;
-	    int count = 0;
-	    
-	    for(int i=l; i<r; i++)
-		if(s.charAt(i) == s.charAt(i+1)) count++;//O(n)
-
-	    System.out.println(count);
-	    
-	    n--;
+	int[] acum = new int[s.length()];
+        acum[0] = 0;
+        for(int i=1; i<acum.length; i++){
+	    acum[i] = acum[i-1];
+	    if(s.charAt(i) == s.charAt(i-1))
+		acum[i]++;
 	}
+
+	// for(int i: acum)
+	//     System.out.print(i);
+
+        int n = sc.nextInt();
+
+        while(n > 0){
+            int l = sc.nextInt() - 1;
+            int r = sc.nextInt() - 1;
+
+            System.out.println(acum[r] - acum[l]);
+            n--;
+        }
     }
     
     public static void main(String[] args){
